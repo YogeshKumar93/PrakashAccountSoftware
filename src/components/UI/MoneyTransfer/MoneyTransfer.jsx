c68dd787167db5799206fe531aa842e6016c0e71
dev <devbiggbrains@gmail.com>
2025-09-17 11:11:19 +0530

    changes

--- src/components/UI/MoneyTransfer/MoneyTransfer.jsx
+++ src/components/UI/MoneyTransfer/MoneyTransfer.jsx
@@ -1,16 +1,51 @@
 import React from "react";
 
 // Icons
-import DescriptionIcon from "@mui/icons-material/Description"; // Template
-import RuleIcon from "@mui/icons-material/Rule"; // Comm Rules
+// import DescriptionIcon from "@mui/icons-material/Description"; // Template
+// import RuleIcon from "@mui/icons-material/Rule"; // Comm Rules
 import Dmt2 from "../../../pages/Dmt2";
 import Dmt from "../../../pages/Dmt";
 import CommonTabs from "../../common/CommonTabs";
+import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
 
 export const MoneyTransfer = () => {
   const tabItems = [
-    { label: "Dmt1", icon: <DescriptionIcon />, component: <Dmt /> },
-    { label: "Dmt2", icon: <RuleIcon />, component: <Dmt2 /> },
+  
+ {
+  label: (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        gap: "6px",
+        minWidth: "80px", // gives proper width
+        justifyContent: "flex-start", // align consistently
+      }}
+    >
+      <CurrencyRupeeIcon fontSize="small" />
+      <span>Dmt1</span>
+    </div>
+  ),
+  component: <Dmt />,
+},
+{
+  label: (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        gap: "6px",
+        minWidth: "80px", // same width for consistency
+        justifyContent: "flex-start",
+      }}
+    >
+      <CurrencyRupeeIcon fontSize="small" />
+      <span>Dmt2</span>
+    </div>
+  ),
+  component: <Dmt2 />,
+},
+
   ];
 
   return <CommonTabs tabs={tabItems} defaultTab={0} />;
